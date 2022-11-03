4e05408ea703c721ff4f9ee8662b1a5a94856aa6
Florian Kugler <mail@floriankugler.com>
2022-11-03 09:39:28 +0100

    Exercise 2 file: exercise3.zip

---
+++ Health/ActivityCell.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+extension ActivityType {
+    var symbol: Image {
+        switch self {
+        case .run: return Image(systemName: "figure.run")
+        case .ride: return Image(systemName: "figure.outdoor.cycle")
+        case .hike: return Image(systemName: "figure.hiking")
+        }
+    }
+}
+
+struct ActivityCell: View {
+    var activity: ActivityItem
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(alignment: .top) {
+                image
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("\(Text(activity.user.name).fontWeight(.bold)) completed a \(activity.type.rawValue)")
+                    HStack(alignment: .firstTextBaseline) {
+                        activity.type.symbol
+                        Text(activity.date, style: .date)
+                            .foregroundColor(.secondary)
+                        Spacer()
+                        Image(systemName: "heart")
+                            .symbolVariant(activity.liked ? .fill : .none)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .font(.footnote)
+        }
+    }
+
+    @ViewBuilder var image: some View {
+        RemoteImage(url: activity.user.imageURL, placeholder: { Color.gray })
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 50, height: 50)
+            .clipShape(Circle())
+    }
+}
+
+struct ActivityCellPreview: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            ActivityCell(activity: ActivityItem.sample[0])
+            ActivityCell(activity: ActivityItem.sample[1])
+        }
+        .padding()
+    }
+}
